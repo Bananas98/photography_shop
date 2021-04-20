bf503cf3c35a66b2f4fc37d1810db9abe3bd89c0
David Nasirov <david.nasirov@aimprosoft.com>
2021-04-20 19:41:28 +0300

    second commit

---
+++ src/main/java/com/example/photography_shop/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.example.photography_shop.config;
+
+
+import com.example.photography_shop.security.JwtAuthenticationFilter;
+import com.example.photography_shop.security.JwtAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Qualifier("userDetailsServiceImpl")
+
+    private final UserDetailsService userDetailsService;
+
+    //Konfiguracja security
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .ignoringAntMatchers("/**")
+                .and()
+                .cors()
+                .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager()))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);//REST jest zawsze STATELESS - nie przechowuje stanu
+        //OpenShift jest STATEFUL
+
+    }
+
+
+    //wskazujemy jaka klasa jest odpowiedzialna za szukanie uzytkownika, jaka klasa
+    // odpowiedzialna jest za haszowanie haseł
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+
+    }
+
+}
